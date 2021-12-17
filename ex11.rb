74dd368b7255e2fae8ed6406e56ee071956f4e2f
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-17 13:30:48 -0500

    my attempt at using gets.chomp

--- ex11.rb
+++ ex11.rb
@@ -1,10 +1,21 @@
 print "How old are you? "
-age= gets.chomp
+age = gets.chomp
 
 print "How tall are you? "
-height= gets.chomp
+height = gets.chomp
 
 print "How much do you weigh? "
-weight= gets.chomp
+weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+#My attempt at using gets.chomp
+
+print "What's your name? "
+name = gets.chomp
+print "What Netflix shows do you like to watch? "
+show = gets.chomp
+print "What's your favourite snack? "
+fav_snack = gets.chomp
+
+puts "Hey, #{name}, should we watch '#{show}' and eat #{fav_snack} this Friday?"
