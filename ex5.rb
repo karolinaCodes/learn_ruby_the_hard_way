2949f8c004315ff2c868661321955dd56162a959
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-17 13:11:29 -0500

    add variables that convert height and weight- from study drills

--- ex5.rb
+++ ex5.rb
@@ -1,7 +1,10 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie in 2009
 my_height = 74 # inches
+my_height_in_centimeter= my_height * 2.54
 my_weight = 180 # lbs
+my_weight_in_kilos=my_weight *  0.4535923
+
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
@@ -12,6 +15,8 @@ puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts my_height_in_centimeter
+puts my_weight_in_kilos
 
 # this line is tricky, try to get it exactly right
 # puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
